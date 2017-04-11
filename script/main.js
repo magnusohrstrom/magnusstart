91967b2f032b0e878700f1554056eaf6a097cc82
magnusohrstrom <magnusohrstrom330@gmail.com>
2017-04-11 19:43:00 +0200

    hejhej

---
+++ script/main.js
@@ -0,0 +1,28 @@
+var detectUpScrollAndToggleHeader = () => {
+  let lastScrollTop = 0;
+  let initialTop = $(window).scrollTop();
+  $(window).on('scroll', function() {
+    st = $(this).scrollTop();
+
+    st <= lastScrollTop||lastScrollTop===0 ? document.getElementById('portfolio').classList.remove('active'):
+      document.getElementById('portfolio').classList.add('active');
+
+    st < 0 ? false:lastScrollTop = st;
+    console.log(lastScrollTop);
+  });
+}
+
+
+var detectScrollLength = (leng, id) => {
+
+  $(window).on('scroll', function() {
+    st = $(this).scrollTop();
+    st <= leng ? document.getElementById(id).classList.remove('active'):
+      document.getElementById(id).classList.add('active');
+    console.log(st);
+  });
+}
+
+detectScrollLength(140,'portfolio');
+detectScrollLength(370,'gh');
+detectScrollLength(480,'facebook');
